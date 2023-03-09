b44a98e5081f694fc315e9f37e605778075b6451
tulvgengenr <871005218@qq.com>
2023-03-09 11:17:19 +0800

    update site and baseRepo of the astro's config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-modern-personal-website.netlify.app',
+  site: 'https://tulvgengenr.github.io',
+  base: '/tulvgengenr.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });
